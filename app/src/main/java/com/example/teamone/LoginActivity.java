4361ed1af216c6e0b41dad3e7001553ae92c9c80
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2021-03-31 22:14:04 +0900

    test commit: base outline

---
+++ app/src/main/java/com/example/teamone/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.teamone;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button signInB = (Button)findViewById(R.id.signInButton);
+    }
+}
---
+++ app/src/main/java/com/example/teamone/groupTable.java
@@ -0,0 +1,14 @@
+package com.example.teamone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class groupTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grouptable);
+    }
+}
