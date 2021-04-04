b2722987e889a775e8943b74b0a5637495819f12
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2021-04-04 21:08:01 +0900

    4/4: set test environment

--- app/src/main/java/com/example/teamone/MainActivity.java
+++ app/src/main/java/com/example/teamone/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.teamone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View selfLayout = (View)findViewById(R.id.mainLayout);
+
+        Button myPageB = (Button)selfLayout.findViewById(R.id.btnMyPage);
+        myPageB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), myPage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button groupB = (Button)selfLayout.findViewById(R.id.btnGroup);
+
+        Button friendB = (Button)selfLayout.findViewById(R.id.btnFriend);
+        friendB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), friendList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button settingB = (Button)selfLayout.findViewById(R.id.btnSetUp);
+        settingB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), settings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
