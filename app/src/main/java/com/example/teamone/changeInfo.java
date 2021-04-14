ed929e6ab509bbb3ab7cc37189312683e6148373
Angeonha <74606584+Angeonha@users.noreply.github.com>
2021-04-14 21:13:40 +0900

    informations in setting change

--- app/src/main/java/com/example/teamone/changeInfo.java
+++ app/src/main/java/com/example/teamone/changeInfo.java
@@ -1,6 +1,29 @@
 package com.example.teamone;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
-public class changeInfo {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class changeInfo extends AppCompatActivity {
+
+    Button btnNickname,btnSchool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changeinfo);
+        View selfLayout = (View) findViewById(R.id.ciLayout);
+
+        btnNickname = (Button)selfLayout.findViewById(R.id.btnNicknameChange);
 
 
+        btnSchool = (Button)selfLayout.findViewById(R.id.btnSchoolChange);
+
+    }
 }
