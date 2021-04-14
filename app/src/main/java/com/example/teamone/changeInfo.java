a8bbeea9913026dcfb3c3c4edf55ccc9ec7ff7c1
Angeonha <74606584+Angeonha@users.noreply.github.com>
2021-04-14 22:26:31 +0900

    informations in setting change

--- app/src/main/java/com/example/teamone/changeInfo.java
+++ app/src/main/java/com/example/teamone/changeInfo.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class changeInfo extends AppCompatActivity {
 
     Button btnNickname,btnSchool;
+    String Nickname,School;
+    EditText nickname,school;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +23,34 @@ public class changeInfo extends AppCompatActivity {
         setContentView(R.layout.activity_changeinfo);
         View selfLayout = (View) findViewById(R.id.ciLayout);
 
+        nickname = (EditText)selfLayout.findViewById(R.id.nickValue);
+        school = (EditText)selfLayout.findViewById(R.id.schoolValue);
+
         btnNickname = (Button)selfLayout.findViewById(R.id.btnNicknameChange);
+        btnNickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nickname = String.valueOf (nickname.getText());
+
+                Intent intent = new Intent(getApplicationContext(), groupMemberAdder.class);
+                intent.putExtra("value",Nickname);
+                startActivity(intent);
+            }
+        });
 
 
+        School = String.valueOf(school.getText());
         btnSchool = (Button)selfLayout.findViewById(R.id.btnSchoolChange);
+        btnSchool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                School = String.valueOf (school.getText());
+
+                Intent intent = new Intent(getApplicationContext(), groupMemberAdder.class);
+                intent.putExtra("value",School);
+                startActivity(intent);
+            }
+        });
 
     }
 }
