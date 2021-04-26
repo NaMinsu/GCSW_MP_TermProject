5dc93a5ada3a9f2dd130fc6d4e9c1e103d569697
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2021-04-26 16:29:10 +0900

    4/26: 설정 액티비티 복구

--- app/src/main/java/com/example/teamone/settings.java
+++ app/src/main/java/com/example/teamone/settings.java
@@ -1,24 +1,59 @@
 package com.example.teamone;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class settings extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+public class settings  extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
+        View selfLayout = (View) findViewById(R.id.miLayout);
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+        Button changeInfo = (Button) selfLayout.findViewById(R.id.btnChangeInfo);
+        changeInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), changeInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        Button myPageB = (Button)selfLayout.findViewById(R.id.btnMyPage);
+        myPageB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button groupB = (Button)selfLayout.findViewById(R.id.btnGroup);
+        groupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), groupList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button friendB = (Button)selfLayout.findViewById(R.id.btnFriend);
+        friendB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), friendList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button settingB = (Button)selfLayout.findViewById(R.id.btnSetUp);
+
+
     }
 }
