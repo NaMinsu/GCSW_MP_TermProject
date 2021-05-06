f40a0885a14ea69d9bf2ed486572c4ae9b5db40a
Angeonha <74606584+Angeonha@users.noreply.github.com>
2021-05-06 20:13:26 +0900

    Read settings information from DB

--- app/src/main/java/com/example/teamone/settings.java
+++ app/src/main/java/com/example/teamone/settings.java
@@ -1,19 +1,33 @@
 package com.example.teamone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class settings  extends AppCompatActivity{
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference Users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,38 @@ public class settings  extends AppCompatActivity{
         setContentView(R.layout.activity_settings);
         View selfLayout = (View) findViewById(R.id.miLayout);
         SharedPreferences sf = getSharedPreferences("Users", MODE_PRIVATE);
+        String MY_EMAIL=sf.getString("Email","");
+        String[] emailID = MY_EMAIL.split("\\.");
+        String DBEmail = emailID[0]+"_"+emailID[1];
+
+        TextView nickname = (TextView)findViewById(R.id.nicknames);
+        TextView email = (TextView)findViewById(R.id.Email);
+        TextView School = (TextView)findViewById(R.id.schools);
+
+        email.setText(FirstAuthActivity.getMyID());
+
+        mDatabase = FirebaseDatabase.getInstance();
+        Users = mDatabase.getReference("users").child(DBEmail);
+        Users.child("nickname").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    String nicknames = String.valueOf(task.getResult().getValue());
+                    nickname.setText(nicknames);
+                }
+            }
+        });
+
+        Users.child("school").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    String schools = String.valueOf(task.getResult().getValue());
+                    School.setText(schools);
+                }
+            }
+        });
+
         Button changeInfo = (Button) selfLayout.findViewById(R.id.btnChangeInfo);
         changeInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +75,8 @@ public class settings  extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+
+
 
         Button myPageB = (Button)selfLayout.findViewById(R.id.btnMyPage);
         myPageB.setOnClickListener(new View.OnClickListener() {
