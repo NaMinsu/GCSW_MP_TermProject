8d5881786d76881586f6c6d713f797b5a292f68b
Angeonha <74606584+Angeonha@users.noreply.github.com>
2021-05-20 18:07:47 +0900

    읽어온 시간표에서 table에 미팅약속시간 만들고 sticker하기

--- app/src/main/java/com/example/teamone/groupTable.java
+++ app/src/main/java/com/example/teamone/groupTable.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -107,8 +108,17 @@ public class groupTable extends AppCompatActivity {
         calculating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                if(timetable.getAllSchedulesInStickers() != null) {
+                    ArrayList<Schedule> forCalculating = new ArrayList<>();
+                    forCalculating = timetable.getAllSchedulesInStickers();
+                    Schedule[] baseSchedule = new Schedule[forCalculating.size()];
+                    int i =0;
+                    for(Schedule s:forCalculating)
+                        baseSchedule[i++]=s;
+                    Schedule createTime = new Schedule();
+                    if(calculate(baseSchedule,createTime,300))
+                        addNew(createTime.getDay(),"",name+"'s meeting",createTime.getStartTime(),createTime.getEndTime());
+                }
             }
         });
 
@@ -149,7 +159,7 @@ public class groupTable extends AppCompatActivity {
         timetable.add(schedules);
     }
 
-    public void calculate(Schedule[] groupSchedule,String code) {
+    public boolean calculate(Schedule[] groupSchedule, Schedule newSchedule, int length) {
 
         int index = groupSchedule.length;
 
@@ -199,14 +209,82 @@ public class groupTable extends AppCompatActivity {
                 merged[i][j] = new Schedule();
             }
             if (indicies[i] != 0) {
-                Merging(day[i], merged[i],indicies[i],code);
-                //
+                System.out.println(i);
+                //요일별로 함수 실행, 그리고 여기서 available이 true가 나오면 true return하기
+                if (Merging(day[i], merged[i], newSchedule, indicies[i], length,i)) {
+                    return true;
+                }
             }
         }
+        return false; //모든요일에서 false가 return되면 return false
     }
 
 
-    public void Merging(Schedule[] days,Schedule[] merging, int index,String code) {
+    public boolean input(Schedule[] personal,Schedule newone){
+
+        int index = personal.length;
+
+        Schedule[][] day = new Schedule[7][index];
+
+        int[] indicies = new int[7];
+
+        for (int j = 0; j < 7; j++){
+            for (int i = 0; i < index; i++) {
+                day[j][i] = new Schedule();
+            }
+        }
+
+        //요일 나누기
+        for (int i = 0; i < index; i++) {
+            if (personal[i].getDay() == 0) {
+                day[0][indicies[0]] = personal[i];
+                indicies[0]++;
+
+            } else if (personal[i].getDay() == 1) {
+                day[1][indicies[1]] = personal[i];
+                indicies[1]++;
+            } else if (personal[i].getDay() == 2) {
+                day[2][indicies[2]] = personal[i];
+                indicies[2]++;
+            } else if (personal[i].getDay() == 3) {
+                day[3][indicies[3]] = personal[i];
+                indicies[3]++;
+            } else if (personal[i].getDay() == 4) {
+                day[4][indicies[4]] = personal[i];
+                indicies[4]++;
+            } else if (personal[i].getDay() == 5) {
+                day[5][indicies[5]] = personal[i];
+                indicies[5]++;
+            } else if (personal[i].getDay() == 6) {
+                day[6][indicies[6]] = personal[i];
+                indicies[6]++;
+            }
+        }
+
+        for(int i = 0; i < 6; i++){
+            if(newone.getDay() == i){
+                for(int j = 0; j < indicies[i];j++){
+                    // 끝나는 시간이 원래 시작시간과 끝 시간 사이일때 false (insert 불가)
+                    if(newone.getEndTime().getHour()*100 + newone.getEndTime().getMinute() >= day[i][j].getStartTime().getHour()*100 + day[i][j].getStartTime().getMinute()
+                            && newone.getEndTime().getHour()*100 + newone.getEndTime().getMinute() <= day[i][j].getEndTime().getHour()*100 + day[i][j].getEndTime().getMinute()
+                    )
+                        return false;
+
+                        //시작시간의 기존의 시간표의 시작시간과 끝 시간 사이에 있을때 false ( insert 불가)
+                    else if(newone.getStartTime().getHour()*100 + newone.getStartTime().getMinute() >= day[i][j].getStartTime().getHour()*100 + day[i][j].getStartTime().getMinute()
+                            && newone.getStartTime().getHour()*100 + newone.getStartTime().getMinute() <= day[i][j].getEndTime().getHour()*100 + day[i][j].getEndTime().getMinute())
+                        return false;
+                }
+            }
+        }
+
+        return true; // true가 오면 TimeTableView의 addnew 사용해서 insert
+    }
+
+
+    //요일별 시간표를 한개로 합치기 -> 예를들어 월요일 9시부터 1시 수업, 다른 그룹멤버의 사간표가 12시부터 2시까지 수업이면 이 일정들을 합해 9시 ~ 2시 로 만들기.
+    //merging 함수에서 합치기
+    public boolean Merging(Schedule[] days,Schedule[] merging,Schedule newschedule, int index,int length,int day) {
 
         Schedule temp = new Schedule();
         int done=0;
@@ -269,13 +347,98 @@ public class groupTable extends AppCompatActivity {
             count++; //한바퀴 다 돌고 아직 모든 array의 value들이 안합쳐졌다면 나머지도 합쳐야함.
         }
 
-        int size = merging.length;
-        for(int i =0;i<size;i++){
-            //이부분에서 grouplist - groupcode 안의 시간표에 양식에 맞게 값 채우기
-        }
+        return available(merging,newschedule,count,length,day);
 
     }//merging function end
 
+
+
+
+
+    //합친 것을 바탕으로 boolean함수로 가능한지 확인하기
+    //length 받은 것을 바탕으로 merge완료 된 각 날짜의 스케줄의 끝나는 시간과 시작시간을 빼면서 length보다 작은 값 나오는 것들 다 찾기.
+    //만약 여기서 true가 나온다면 전체 함수 끝
+    public boolean available(Schedule[] days, Schedule sample, int index,int length,int day){
+
+        sample.setDay(day);
+
+        if(index == 1) { // 한개로 전체가 merging된 경우 시작시간 - 9시 or 10시 - 끝난시간 해서 이게 length 안이면 return true
+
+            if(days[0].getStartTime().getHour()*100 + days[0].getStartTime().getMinute() - 900 >= length) {
+                sample.setStartTime(new Time(9,0));
+                int hour = length/100;
+                int minute = length%100;
+                sample.setEndTime(new Time(sample.getStartTime().getHour()+hour,sample.getStartTime().getMinute()+minute));
+                return true;
+            }
+
+            else if(2200 - days[0].getEndTime().getHour()*100 + days[0].getEndTime().getMinute() >= length) {
+                sample.setStartTime(days[0].getEndTime());
+                int hour = length/100;
+                int minute = length%100;
+                sample.setEndTime(new Time(sample.getStartTime().getHour()+hour,sample.getStartTime().getMinute()+minute));
+                return true;
+            }
+
+            else return false;
+        }
+
+        else if(index > 1){   // index가 2개 이상이면 index 0 의 시작시간 - 9시 해서 length 안쪽이면 return true 아닐 경우  (index i+1 시작 시간) - (index i 끝시간) 일 경우 return true, 아니면 10시(default 마지막 시간) - (index i 끝시간) 이면 return true
+
+
+            if(days[0].getStartTime().getHour()*100 + days[0].getStartTime().getMinute() - 900 >= length) {
+                sample.setStartTime(new Time(9,0));
+                int hour = length/100;
+                int minute = length%100;
+                sample.setEndTime(new Time(sample.getStartTime().getHour()+hour,sample.getStartTime().getMinute()+minute));
+                return true;
+            }
+
+            for(int i =0 ; i < index -1 ; i++) {
+
+
+                int hour = days[i+1].getStartTime().getHour() - days[i].getEndTime().getHour();
+                int minute = days[i+1].getStartTime().getMinute() - days[i].getEndTime().getMinute();
+
+                if(minute < 0) {
+                    hour--;
+                    minute = 60 + minute;
+                }//60분이기 때문에 그에 맞춰서 다시 계산해주기
+
+
+                if(hour * 100 + minute >= length) {
+
+                    sample.setStartTime(new Time(days[i].getEndTime().getHour(),days[i].getEndTime().getMinute()));
+
+                    int endMinute = days[i].getEndTime().getMinute() + length%100;
+                    int endHour = days[i].getEndTime().getHour() + length/100;
+                    if(endMinute >= 60) {
+                        endHour++;
+                        endMinute = endMinute-60;
+                    }
+
+                    sample.setEndTime(new Time(endHour,endMinute));
+
+                    return true;
+
+                }
+            }
+
+            if(2200 - days[index-1].getEndTime().getHour()*100 + days[index-1].getEndTime().getMinute() >= length) {
+                sample.setStartTime(days[index-1].getEndTime());
+                int hour = length/100;
+                int minute = length%100;
+                sample.setEndTime(new Time(sample.getStartTime().getHour()+hour,sample.getStartTime().getMinute()+minute));
+                return true;
+            }
+
+        }
+
+        else
+            return false; //이 요일에 schedule가 아예 없는 경우
+
+        return false; //위 경우 모두 아니면 return false
+    }
 
 
 }
